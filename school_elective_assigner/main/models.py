205dd0f5377431215d3428c1de683d64e9dba671
Marcus Miyalys <resolutions@riseup.net>
2021-05-24 00:58:17 +0200

    Defaults for BooleanFields

--- school_elective_assigner/main/models.py
+++ school_elective_assigner/main/models.py
@@ -19,7 +19,7 @@ class Course(models.Model):
   name_da = models.CharField(max_length=200)
   description_en = models.TextField()
   description_da = models.TextField()
-  active = models.BooleanField()
+  active = models.BooleanField(default=True)
   deadline = models.DateField()
 
   def __str__(self):
@@ -58,7 +58,7 @@ class Assignment(models.Model):
   invitation_email = models.TextField(max_length=200, null=True, blank=True)
   reminder_email = models.TextField(null=True, blank=True )
   priority_form_text = models.TextField(null=True, blank=True)
-  emails_sent = models.BooleanField()
+  emails_sent = models.BooleanField(default=False)
 
   def __str__(self):
     return self.name
