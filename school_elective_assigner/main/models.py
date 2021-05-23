6cd1a76a7d4bf18473869903ab581c0d5524c018
Marcus Miyalys <resolutions@riseup.net>
2021-05-23 23:48:31 +0200

    Models first version done, template and css files loading

--- school_elective_assigner/main/models.py
+++ school_elective_assigner/main/models.py
@@ -1,29 +1,64 @@
 from django.db import models
 
-
 class Student(models.Model):
   first_name = models.CharField(max_length=200)
-  login_code = models.CharField(max_length=75, null=True)
+  login_code = models.CharField(max_length=75, null=True, blank=True)
   #imdb_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
+
+  def __str__(self):
+    return self.first_name
 
 class Teacher(models.Model):
   full_name = models.CharField(max_length=200)
 
+  def __str__(self):
+    return self.full_name
+
 class Course(models.Model):
-  title = models.CharField(max_length=200)
+  name_en = models.CharField(max_length=200)
+  name_da = models.CharField(max_length=200)
+  description_en = models.TextField()
+  description_da = models.TextField()
+  active = models.BooleanField()
+  deadline = models.DateField()
+
+  def __str__(self):
+    return self.name_da
 
 class Student_Course_Request(models.Model):
-  priority = models.
+  priority = models.PositiveSmallIntegerField()
 
-#
+  def __str__(self):
+    return self.priority
+
+# Only has foreign keys
 class Student_Course_Assigned(models.Model):
   pass
 
-class Rules(models.Model):
-  title = models.CharField(max_length=200)
+  #def __str__(self):
+  #  return self.name
+
+class Rule(models.Model):
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
 
 class School(models.Model):
-  title = models.CharField(max_length=200)
-  j
+  name = models.CharField(max_length=100)
+  password = models.CharField(max_length=200)
+  logo = models.FileField(upload_to="uploads/logos", null=True, blank=True)
+  email_footer = models.TextField(null=True, blank=True)
+
+  def __str__(self):
+    return self.name
+
 class Assignment(models.Model):
-  title = models.CharField(max_length=200)
+  name = models.CharField(max_length=200)
+  invitation_email = models.TextField(max_length=200, null=True, blank=True)
+  reminder_email = models.TextField(null=True, blank=True )
+  priority_form_text = models.TextField(null=True, blank=True)
+  emails_sent = models.BooleanField()
+
+  def __str__(self):
+    return self.name
--- school_elective_assigner/school_elective_assigner/settings.py
+++ school_elective_assigner/school_elective_assigner/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'school_elective_assigner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'common/templates/' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# miyalys: don't know if this is best practices anymore?
+STATICFILES_DIRS = [
+    BASE_DIR / 'common/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
