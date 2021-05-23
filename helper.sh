44a92d629e0e2a0c5848c87c8fd178637ed20092
Marcus Miyalys <resolutions@riseup.net>
2021-05-24 00:45:54 +0200

    Update helper with migrations, migrate, add models to django admin

--- helper.sh
+++ helper.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 
-if [ -z "$1" ]; then exit; fi
+if [ -z "$1" ]; then 
+  exit
+else
+  # Load the virtual env first
+  . env/bin/activate
 
-if [ "$1" = 'runl' ]; then
-  . env/bin/activate && school_elective_assigner/manage.py runserver
-elif [ "$1" = 'runw' ]; then
-  . env/Scripts/activate && python.exe school_elective_assigner/manage.py runserver
+  if [ "$1" = 'runl' ]; then
+    school_elective_assigner/manage.py runserver
+  elif [ "$1" = 'runw' ]; then
+    python.exe school_elective_assigner/manage.py runserver
+  elif [ "$1" = 'migrate' ]; then
+    school_elective_assigner/manage.py makemigrations
+    school_elective_assigner/manage.py migrate
+  fi
 fi
--- school_elective_assigner/main/admin.py
+++ school_elective_assigner/main/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import (
+  Student, Teacher, Course, Student_Course_Request,
+  Student_Course_Assigned, Rule, School, Assignment)
 
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Student_Course_Request)
+class Student_Course_RequestAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Student_Course_Assigned)
+class Student_Course_AssignedAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Rule)
+class RuleAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+  pass
